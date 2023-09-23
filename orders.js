4f7aa428035259f02b89c419c975d0cb07448204
gaurav pagare <pagaregaurav862@gmail.com>
2023-09-23 16:45:23 +0530

    Dashboard

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber:'36378',
+        paymentStatus: 'Refund',
+        Shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Pro Drone',
+        productNumber:'49347',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera Drone',
+        productNumber:'96996',
+        paymentStatus: 'Paid',
+        Shipping: 'Deliverd'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber:'22821',
+        paymentStatus: 'Paid',
+        Shipping: 'Deliverd'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber:'81475',
+        paymentStatus: 'Due',
+        Shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber:'00482',
+        paymentStatus: 'Paid',
+        Shipping: 'Delivered'
+    },
+
+]
